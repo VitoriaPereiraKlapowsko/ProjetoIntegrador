edfba4d9ac49ca32310befdcc5e260226fea66b3
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2023-11-19 21:57:16 -0300

    Quase conseguindo fazer funcionar o editar pet

--- src/main/java/ifpr/pgua/eic/colecaomusicas/controllers/ListarPet.java
+++ src/main/java/ifpr/pgua/eic/colecaomusicas/controllers/ListarPet.java
@@ -9,6 +9,7 @@ import com.github.hugoperlin.results.Resultado;
 import ifpr.pgua.eic.colecaomusicas.App;
 import ifpr.pgua.eic.colecaomusicas.models.Pet;
 import ifpr.pgua.eic.colecaomusicas.models.Raca;
+import ifpr.pgua.eic.colecaomusicas.models.Status;
 import ifpr.pgua.eic.colecaomusicas.repositories.RepositorioPet;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -18,7 +19,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.ListView;
 
-public class ListarPet implements Initializable{
+public class ListarPet implements Initializable {
 
     @FXML
     private ListView<Pet> listaPets;
@@ -58,13 +59,16 @@ public class ListarPet implements Initializable{
     @FXML
     void alterar(ActionEvent event) {
         if (selecionado != null) {
-            App.pushScreen("CADASTROPET",o-> new CadastroPet(repositorio, selecionado));
+            App.pushScreen("CADASTROPET", o -> new CadastroPet(repositorio, selecionado));
         }
     }
-    
+
     @FXML
-    void selecionar(MouseEvent event) {
-        selecionado = listaPets.getSelectionModel().getSelectedItem();
+    private void selecionar() {
+        Pet itemSelecionado = listaPets.getSelectionModel().getSelectedItem();
+        if (itemSelecionado != null) {
+            selecionado = itemSelecionado;
+        }
     }
 
     @Override
@@ -72,11 +76,11 @@ public class ListarPet implements Initializable{
         listaPets.getItems().clear();
         Resultado resultado = repositorio.listarPet();
 
-        if(resultado.foiErro()){
+        if (resultado.foiErro()) {
             Alert alert = new Alert(AlertType.ERROR, resultado.getMsg());
             alert.showAndWait();
-        }else{
-            List lista = (List)resultado.comoSucesso().getObj();
+        } else {
+            List lista = (List) resultado.comoSucesso().getObj();
             listaPets.getItems().addAll(lista);
         }
     }
